d69b62f7ecc29d5b8c4939250777d14b91a5bdba
James Adler <jamespadler@gmail.com>
2021-10-21 18:27:35 -0700

    converting deduper file to correct name

--- adler_deduper.py
+++ adler_deduper.py
@@ -6,22 +6,36 @@ import argparse
 
 # define arguments
 parser = argparse.ArgumentParser(description="Reference-based PCR duplicate removal from mapped SAM file")
-parser.add_argument("-f", "--file", help="Input SAM file - sorted, mapped reads")
-parser.add_argument("-p", "--paired-end", action="store_true", help="Designates file is paired end. If flag is not set, the program will assume single end reads.")
-parser.add_argument("-u", "--umi", help="Designates UMI file. If set, UMI file should follow flag. If flag is not set, the program will assume randomer UMI")
+parser.add_argument(
+    "-f",
+    "--input_file", 
+    help="Input SAM file - sorted, mapped reads")
+parser.add_argument(
+    "-p", 
+    "--paired-end", 
+    action="store_true", 
+    help="Designates file is paired end. If flag is not set, the program will assume single end reads.")
+parser.add_argument(
+    "-u", 
+    "--umi", 
+    help="Designates UMI file. If set, UMI file should follow flag. If flag is not set, the program will assume randomer UMI")
 args = parser.parse_args()
 
 # define globals
 paired_end = False
-input_file = args.file
+input_file = args.input_file
+print("Input SAM file:", input_file, sep=" ")
 
 # argparser
 if args.paired_end:
     paired_end = True
-    print(input_file)
+    exit("Apologies! Paired-end functionality not yet included. Please remove paired_end flag (-p, --paired_end). Exiting script.")
 if args.umi:
     umis = args.umi
-    print(umis)
+    print("UMI file:", umis, sep=" ")
+else:
+    exit("Apologies! Randomer functionality not yet included. Please utilize umi flag (-u, --umi) and specify UMI file. Exiting script.")
+
 
 # define clases
 class SamFile():
